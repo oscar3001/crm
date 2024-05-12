4c2dd6e6800070cd3608a04388e5e51123477843
oscar3001 <38573206+oscar3001@users.noreply.github.com>
2024-05-12 08:05:50 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -52,7 +52,11 @@ const path = require("path");
 
 const currentDir = process.cwd();
 
+app.use(express.static(path.resolve(currentDir, "./client/public")));
 
+app.get("*", function (request, response) {
+    response.sendFile(path.resolve(currentDir, "./client/public", "index.html"));
+});
 
 const server = app.listen(process.env.PORT || 3010, () => {
     console.log(`WaCrm server is running on port ${process.env.PORT}`);
